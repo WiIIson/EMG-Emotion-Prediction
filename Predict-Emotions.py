b536157b757ddec500c54c349b5064d87201d72c
WiIIson <69092314+WiIIson@users.noreply.github.com>
2026-03-24 19:19:24 -0400

    Create Predict-Emotions.py
    Created basic emotional predictor

---
+++ Predict-Emotions.py
@@ -0,0 +1,111 @@
+import torch
+import pandas as pd
+import numpy as np
+
+"""
+01 - Neutral
+02 - Calm
+03 - Happy
+04 - Sad
+05 - Angry
+06 - Fearful
+07 - Disgust
+
+Capture filename:
+Capture
+"""
+
+def load_data(p=0.1, batch_size=25):
+    # Load/clean
+    df = pd.read_csv('Features.csv')
+    df['id'] = df['File_Path'].str.extract(r'(\d{2}-\d{2}-\d{2})')[0].str[3:5].astype(int) - 1
+    # Split into train/test dataframe
+    train_df = df.sample(frac=1-p, random_state=42)
+    test_df = df.drop(train_df.index)
+    # Create trainloader
+    y_train = train_df['id']
+    X_train = train_df.drop(['File_Path', 'id'], axis=1)
+    y_train = torch.from_numpy(y_train.to_numpy(dtype=np.long))
+    X_train = torch.from_numpy(X_train.to_numpy(dtype=np.float32))
+    trainset = torch.utils.data.TensorDataset(X_train, y_train)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
+    # Create testloader
+    y_test = test_df['id']
+    X_test = test_df.drop(['File_Path', 'id'], axis=1)
+    y_test = torch.from_numpy(y_test.to_numpy(dtype=np.long))
+    X_test = torch.from_numpy(X_test.to_numpy(dtype=np.float32))
+    testset = torch.utils.data.TensorDataset(X_test, y_test)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=True)
+
+    return trainloader, testloader
+    
+    
+# Basic training method, feel free to change the criterion/optimizer as necessary
+def train_model(trainloader, model, epochs=10):
+    
+    # Set criterion and optimizer
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+    # Main training loop
+    for epoch in range(epochs):
+        epoch_loss = 0
+
+        for X, Y in trainloader:
+            # Forward pass
+            outputs = model(X)
+            loss = criterion(outputs, Y.long())
+            # Backward pass and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            # Accumulate loss
+            epoch_loss += float(loss.item())
+        
+        # Print epoch stats
+        epoch_loss /= len(trainloader)
+        print(f'Epoch: [{epoch+1}/{epochs}], Loss: [{epoch_loss:.2f}]')
+
+# Basic testing method
+def test_model(testloader, model):
+    criterion = torch.nn.CrossEntropyLoss()
+    epoch_loss = 0
+    all_labels = []
+    all_predictions = []
+
+    model.eval()
+    with torch.no_grad():
+        for X, Y in testloader:
+            outputs = model(X)
+            loss = criterion(outputs, Y.long())
+            epoch_loss += loss.item()
+
+            all_labels.append(Y.cpu())
+            all_predictions.append(torch.argmax(outputs, dim=1).cpu())
+    
+    epoch_loss /= len(testloader)
+    print(f"Test Loss: [{epoch_loss:.2f}]")
+
+    all_labels = torch.cat(all_labels)
+    all_predictions = torch.cat(all_predictions)
+    df = pd.DataFrame({'labels':all_labels.numpy(), 'predictions':all_predictions.numpy()})
+    
+    return df, epoch_loss
+
+# Basic classification model, tweak this later to get better accuracy
+class EmotionPredictionModel(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = torch.nn.Linear(70, 160)
+        self.fc2 = torch.nn.Linear(160, 7)
+
+    def forward(self, x):
+        x = torch.nn.functional.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+if __name__ == '__main__':
+    model = EmotionPredictionModel()
+    trainloader, testloader = load_data()
+    train_model(trainloader, model, 50)
+    df, epoch_loss = test_model(testloader, model)
